ac40d82f9eea758894f5281ef0e185a5704d195e
kira07sj <kirubeldagne1@gmail.com>
2024-11-28 15:23:15 +0300

    Setup deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enable static export
+    output: 'export', // Enable static exports
+    images: {
+      unoptimized: true, // Disable Image Optimization API
+    },
   };
   
   export default nextConfig;
